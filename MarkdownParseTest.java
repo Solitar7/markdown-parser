916c77069cf55d7aa194e750f25b46efe7b4731c
Solitar7 <y1wen@ucsd.edu>
2022-04-21 23:54:01 +0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void simpleCheck() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> simpleLinks = MarkdownParse.getLinks(content);
+        assertEquals("https://something.com", simpleLinks.get(0));
+        assertEquals("some-thing.html", simpleLinks.get(1));
     }
 }
