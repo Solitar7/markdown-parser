fe1b62d57580ccda93b252d81255d35f11442423
Solitar7 <y1wen@ucsd.edu>
2022-05-23 07:57:55 +0800

    Create MarkdownParseTester.java

---
+++ MarkdownParseTester.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+public class MarkdownParseTester {
+    
+    @Test
+    public void checkSnippet1() throws IOException{}
+
+    @Test
+    public void checkSnippet2() throws IOException{}
+
+    @Test
+    public void checkSnippet3() throws IOException{}
+}
