7350854af9b54091a56bcfd241eb162b4e531ac0
Solitar7 <y1wen@ucsd.edu>
2022-05-23 08:23:45 +0800

    Update MarkdownParseTester.java

--- MarkdownParseTester.java
+++ MarkdownParseTester.java
@@ -7,11 +7,36 @@ import java.nio.file.Path;
 public class MarkdownParseTester {
     
     @Test
-    public void checkSnippet1() throws IOException{}
+    public void checkSnippet1() throws IOException{
+        Path fileName = Path.of("Snippet1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> linksGet = MarkdownParse.getLinks(content);
+        ArrayList<String> checkList = new ArrayList<String>();
+        checkList.add("`google.com");
+        checkList.add("google.com");
+        checkList.add("ucsd.edu");
+        assertEquals(checkList, linksGet);
+    }
 
     @Test
-    public void checkSnippet2() throws IOException{}
+    public void checkSnippet2() throws IOException{
+        Path fileName = Path.of("Snippet2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> linksGet = MarkdownParse.getLinks(content);
+        ArrayList<String> checkList = new ArrayList<String>();
+        checkList.add("a.com");
+        checkList.add("a.com(())");
+        checkList.add("example.com");
+        assertEquals(checkList, linksGet);
+    }
 
     @Test
-    public void checkSnippet3() throws IOException{}
+    public void checkSnippet3() throws IOException{
+        Path fileName = Path.of("Snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> linksGet = MarkdownParse.getLinks(content);
+        ArrayList<String> checkList = new ArrayList<String>();
+        checkList.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        assertEquals(checkList, linksGet);
+    }
 }
